cf1ed34113b5c6b2181ab287046b6f4926176621
jofas <jonas@fc-web.de>
2022-05-22 13:48:33 +0100

    julia set zoom

--- src/julia_set.rs
+++ src/julia_set.rs
@@ -6,22 +6,33 @@ use rayon::iter::{
 };
 
 fn main() {
-  let imgx = 1080;
+  //let imgx = 7680;
+  //let imgy = 4320;
+
+  let imgx = 1920;
   let imgy = 1080;
 
   let mut imgbuf = RgbaImage::new(imgx, imgy);
 
-  let scalex = 3.0 / imgx as f32;
-  let scaley = 3.0 / imgy as f32;
+  let (norm_x, norm_y) = if imgx > imgy {
+    (imgx as f32 / imgy as f32, 1.)
+  } else {
+    (1., imgy as f32 / imgx as f32)
+  };
 
   let c = num_complex::Complex::new(-0.4, 0.6);
 
+  let zoom = 0.01;
+
+  let zpx = 0.14;
+  let zpy = -0.4;
+
   for x in 0..imgx {
     for y in 0..imgy {
-      let cx = x as f32 * scalex - 1.5;
-      let cy = y as f32 * scaley - 1.5;
+      let zx = (x as f32 / imgx as f32 * 2. * zoom - zoom + zpx) * norm_x;
+      let zy = (y as f32 / imgy as f32 * 2. * zoom - zoom + zpy) * norm_y;
 
-      let mut z = num_complex::Complex::new(cx, cy);
+      let mut z = num_complex::Complex::new(zx, zy);
 
       let mut i = 0;
       while i < 255 && z.norm() <= 2.0 {
