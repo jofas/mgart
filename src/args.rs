33a0661b3f8e6d830d799f6ff0a94b9fda15a8c9
jofas <jonas@fc-web.de>
2022-05-23 11:03:19 +0100

    better default interface

--- src/args.rs
+++ src/args.rs
@@ -2,32 +2,32 @@ use clap::Args;
 
 use serde::Deserialize;
 
-use crate::util::{ColorMap1D, RgbaColor};
+use crate::util::ColorMap1D;
 
 #[derive(Args, Deserialize)]
 pub struct JuliaSetArgs {
-  #[clap(default_value = "1920")]
+  #[clap(default_value_t = default_width())]
   #[serde(default = "default_width")]
   pub width: u32,
-  #[clap(default_value = "1080")]
+  #[clap(default_value_t = default_height())]
   #[serde(default = "default_height")]
   pub height: u32,
-  #[clap(default_value = "0.5")]
+  #[clap(default_value_t = default_zoom())]
   #[serde(default = "default_zoom")]
   pub zoom: f32,
-  #[clap(default_value = "0.")]
+  #[clap(default_value_t = default_zpx())]
   #[serde(default = "default_zpx")]
   pub zpx: f32,
-  #[clap(default_value = "0.")]
+  #[clap(default_value_t = default_zpy())]
   #[serde(default = "default_zpy")]
   pub zpy: f32,
-  #[clap(default_value = "100")]
+  #[clap(default_value_t = default_iter())]
   #[serde(default = "default_iter")]
   pub iter: u32,
-  #[clap(default_value = "julia_set.png")]
+  #[clap(default_value_t = default_filename())]
   #[serde(default = "default_filename")]
   pub filename: String,
-  #[clap(default_value_t = ColorMap1D::new(vec![]))]
+  #[clap(default_value_t = default_color_map())]
   #[serde(default = "default_color_map")]
   pub color_map: ColorMap1D,
 }
@@ -61,8 +61,5 @@ fn default_filename() -> String {
 }
 
 fn default_color_map() -> ColorMap1D {
-  ColorMap1D::new(vec![
-    RgbaColor::new_hex(0xFFFFFFFF),
-    RgbaColor::new_hex(0x000000FF),
-  ])
+  ColorMap1D::new(vec![])
 }
