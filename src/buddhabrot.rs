a4e8f1f6b42ea815cda282ef10ba81de73cc6b29
jofas <jonas@fc-web.de>
2022-11-23 16:13:36 +0100

    InterpolationTiles

--- src/buddhabrot.rs
+++ src/buddhabrot.rs
@@ -124,7 +124,7 @@ impl Buddhabrot {
       buffer.into_iter().map(|x| x.into_inner() as f64).collect();
 
     for process in self.post_processing {
-      process.apply(&mut buffer, self.width, self.height);
+      process.apply(&mut buffer, self.width, self.height)?;
     }
 
     info!("post processing done");
