e321eec098c73dff067d7bcd225df9e5669fbba3
jofas <jonas@fc-web.de>
2022-11-30 15:26:30 +0100

    [no ci] no clippy possible wraps going on anymore

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
 #![allow(clippy::cast_sign_loss)]
 #![allow(clippy::cast_lossless)]
 #![allow(clippy::cast_precision_loss)]
-#![allow(clippy::cast_possible_wrap)]
 #![allow(clippy::cast_possible_truncation)]
 
 use serde::Deserialize;
