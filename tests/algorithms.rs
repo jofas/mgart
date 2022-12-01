4b5468dab75b7e569ee651d4818248fd9f5bd094
jofas <jonas@fc-web.de>
2022-12-01 15:32:42 +0100

    test for buddhabrot

---
+++ tests/algorithms.rs
@@ -0,0 +1,26 @@
+use mgart::buddhabrot::Buddhabrot;
+use mgart::util::coloring::ColorMap1d;
+use mgart::util::sampler::Sampler;
+use mgart::util::ComplexNumber;
+use mgart::util::post_processing::PostProcessing;
+
+#[test]
+fn buddhabrot() {
+  let buddhabrot = Buddhabrot::new(
+    1920,
+    1024,
+    ComplexNumber::Cartesian { re: 0., im: 0. },
+    1.,
+    200,
+    None,
+    ColorMap1d::default(),
+    2.,
+    100_000,
+    Sampler::UniformPolar { r: 2. },
+    vec![
+      PostProcessing::Normalize,
+    ],
+  );
+
+  buddhabrot.creator().create();
+}
